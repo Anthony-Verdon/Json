190d8c3fbb976df1bacbdd68a4c6cda2385feca5
Anthony Verdon <anthonyverdon10@gmail.com>
2025-11-21 11:05:45 +0100

    add WriteFile function

--- srcs/Json/Json.hpp
+++ srcs/Json/Json.hpp
@@ -2,3 +2,23 @@
 
 #include "Json/Node/Node.hpp"
 #include "Json/Parser/Parser.hpp"
+#include <fstream>
+#include <iostream>
+
+namespace Json
+{
+inline void WriteFile(const std::string &path, const Node &node)
+{
+    try
+    {
+        std::ofstream file;
+        file.open(path);
+        file << node;
+        file.close();
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << "failed to write " << path << " : " << e.what() << std::endl;
+    }
+}
+} // namespace Json
